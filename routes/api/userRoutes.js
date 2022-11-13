5aa6a8b54de45509ccf62d8f07ccf2be87a06550
nhan <nhanng19@gmail.com>
2022-11-13 14:56:56 -0800

    user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addUserFriend,
+  removeUserFriend,
+} = require("../../controller/userController");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .post(addUserFriend)
+  .delete(removeUserFriend);
+
+module.exports = router;
