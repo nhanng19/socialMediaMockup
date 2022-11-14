cefc8372aa821bbddfdc8414355f06f82e0b0594
nhan <nhanng19@gmail.com>
2022-11-13 23:29:48 -0800

    added a feature to populate user's thoughts and thoughts' reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,9 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  getReactions,
+  createReaction,
+  deleteReaction,
 } = require("../../controller/thoughtController");
 
 router.route("/").get(getThoughts).post(createThought);
@@ -15,5 +18,12 @@ router
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+  .get(getReactions)
+  .post(createReaction)
+
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction)
 
 module.exports = router;
