6d3525cf65b15e160c8924c74190c85b0d283b1a
nhan <nhanng19@gmail.com>
2022-11-16 02:30:24 -0800

    provided comments

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const { connect, connection } = require("mongoose");
 
+// Sets up mongoDB to our localhost;
+
 connect("mongodb://localhost/socialMediaDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
