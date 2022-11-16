758355a23de9c20ec36a87227ac960d5c07a96e8
nhan <nhanng19@gmail.com>
2022-11-16 02:32:24 -0800

    provided quality comments for users' routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,12 +9,12 @@ const {
   removeUserFriend,
 } = require("../../controller/userController");
 
-router.route("/").get(getUsers).post(createUser);
+router.route("/").get(getUsers).post(createUser); // GET and POST a user at localhost:3001/api/users;
 
-router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser); // GET, PUT, and DELETE a user at localhost:3001/api/users;
 
 router
-  .route("/:userId/friends/:friendId")
+  .route("/:userId/friends/:friendId") // POST and DELETE to add friends through userID and friendId at localhost:3001/api/users/:userId/friends/:friendId;
   .post(addUserFriend)
   .delete(removeUserFriend);
 
